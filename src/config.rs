ca6771e1028e541083302737ee16421240fda419
Leo-Besancon <leoleo38@live.fr>
2023-06-07 16:39:56 +0200

    Initial update - use massa-db and massa-final-state

---
+++ src/config.rs
@@ -0,0 +1,101 @@
+use massa_async_pool::AsyncPoolConfig;
+use massa_db_exports::MassaDBConfig;
+use massa_executed_ops::{ExecutedDenunciationsConfig, ExecutedOpsConfig};
+use massa_final_state::FinalStateConfig;
+use massa_ledger_exports::LedgerConfig;
+use massa_models::config::{
+    DENUNCIATION_EXPIRE_PERIODS, ENDORSEMENT_COUNT, GENESIS_TIMESTAMP, INITIAL_DRAW_SEED,
+    MAX_ASYNC_MESSAGE_DATA, MAX_ASYNC_POOL_LENGTH, MAX_BOOTSTRAPPED_NEW_ELEMENTS,
+    MAX_DATASTORE_KEY_LENGTH, MAX_DATASTORE_VALUE_LENGTH, MAX_DEFERRED_CREDITS_LENGTH,
+    MAX_DENUNCIATIONS_PER_BLOCK_HEADER, MAX_DENUNCIATION_CHANGES_LENGTH,
+    MAX_PRODUCTION_STATS_LENGTH, MAX_ROLLS_COUNT_LENGTH, MIP_STORE_STATS_BLOCK_CONSIDERED,
+    MIP_STORE_STATS_COUNTERS_MAX, PERIODS_PER_CYCLE, POS_SAVED_CYCLES, T0, THREAD_COUNT,
+};
+use massa_pos_exports::PoSConfig;
+use massa_versioning::versioning::MipStatsConfig;
+use std::path::PathBuf;
+
+pub fn get_db_config(path: PathBuf) -> MassaDBConfig {
+    MassaDBConfig {
+        path,
+        max_history_length: 100,
+        max_new_elements: MAX_BOOTSTRAPPED_NEW_ELEMENTS as usize,
+        thread_count: THREAD_COUNT,
+    }
+}
+
+pub fn get_ledger_config(path: PathBuf) -> LedgerConfig {
+    LedgerConfig {
+        thread_count: THREAD_COUNT,
+        disk_ledger_path: path,
+        initial_ledger_path: PathBuf::new(),
+        max_key_length: MAX_DATASTORE_KEY_LENGTH,
+        max_datastore_value_length: MAX_DATASTORE_VALUE_LENGTH,
+    }
+}
+
+fn get_async_pool_config() -> AsyncPoolConfig {
+    AsyncPoolConfig {
+        max_length: MAX_ASYNC_POOL_LENGTH,
+        thread_count: THREAD_COUNT,
+        max_async_message_data: MAX_ASYNC_MESSAGE_DATA,
+        max_key_length: MAX_DATASTORE_KEY_LENGTH as u32,
+    }
+}
+
+fn get_pos_config() -> PoSConfig {
+    PoSConfig {
+        periods_per_cycle: PERIODS_PER_CYCLE,
+        thread_count: THREAD_COUNT,
+        cycle_history_length: POS_SAVED_CYCLES,
+        max_rolls_length: MAX_ROLLS_COUNT_LENGTH,
+        max_production_stats_length: MAX_PRODUCTION_STATS_LENGTH,
+        max_credit_length: MAX_DEFERRED_CREDITS_LENGTH,
+    }
+}
+
+fn get_executed_ops_config() -> ExecutedOpsConfig {
+    ExecutedOpsConfig {
+        thread_count: THREAD_COUNT,
+    }
+}
+
+fn get_executed_denunciations_config() -> ExecutedDenunciationsConfig {
+    ExecutedDenunciationsConfig {
+        denunciation_expire_periods: DENUNCIATION_EXPIRE_PERIODS,
+        thread_count: THREAD_COUNT,
+        endorsement_count: ENDORSEMENT_COUNT,
+    }
+}
+
+pub fn get_mip_stats_config() -> MipStatsConfig {
+    MipStatsConfig {
+        block_count_considered: MIP_STORE_STATS_BLOCK_CONSIDERED,
+        counters_max: MIP_STORE_STATS_COUNTERS_MAX,
+    }
+}
+
+pub fn get_final_state_config(path: PathBuf) -> FinalStateConfig {
+    let ledger_config = get_ledger_config(path);
+    let async_pool_config = get_async_pool_config();
+    let pos_config = get_pos_config();
+    let executed_ops_config = get_executed_ops_config();
+    let executed_denunciations_config = get_executed_denunciations_config();
+    FinalStateConfig {
+        ledger_config,
+        async_pool_config,
+        pos_config,
+        executed_ops_config,
+        executed_denunciations_config,
+        final_history_length: 100,
+        thread_count: THREAD_COUNT,
+        periods_per_cycle: PERIODS_PER_CYCLE,
+        initial_seed_string: INITIAL_DRAW_SEED.into(),
+        initial_rolls_path: "".into(),
+        endorsement_count: ENDORSEMENT_COUNT,
+        max_executed_denunciations_length: MAX_DENUNCIATION_CHANGES_LENGTH,
+        max_denunciations_per_block_header: MAX_DENUNCIATIONS_PER_BLOCK_HEADER,
+        t0: T0,
+        genesis_timestamp: *GENESIS_TIMESTAMP,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use massa_ledger_exports::{
-    LedgerChanges, LedgerConfig, LedgerController, LedgerEntry, SetUpdateOrDelete,
+use massa_db_exports::{DBBatch, MassaDBController, ShareableMassaDBController};
+use massa_db_worker::MassaDB;
+use massa_final_state::FinalState;
+use massa_ledger_editor::{
+    get_db_config, get_final_state_config, get_ledger_config, get_mip_stats_config,
 };
+use massa_ledger_exports::{LedgerChanges, LedgerEntry, SetUpdateOrDelete};
 use massa_ledger_worker::FinalLedger;
-use massa_models::{
-    address::Address,
-    amount::Amount,
-    bytecode::Bytecode,
-    config::{MAX_DATASTORE_KEY_LENGTH, MAX_DATASTORE_VALUE_LENGTH, THREAD_COUNT},
-    prehash::PreHashMap,
-    slot::Slot,
-};
-use std::{collections::BTreeMap, path::PathBuf, str::FromStr};
+use massa_models::{address::Address, amount::Amount, bytecode::Bytecode, prehash::PreHashMap};
+use massa_pos_exports::test_exports::MockSelectorController;
+use massa_versioning::{mips::MIP_LIST, versioning::MipStore};
+use parking_lot::RwLock;
+use std::{collections::BTreeMap, path::PathBuf, str::FromStr, sync::Arc};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -19,34 +19,83 @@ pub struct Args {
     path: PathBuf,
 }
 
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn convert_from_testnet22_ledger_to_testnet23_ledger(db: ShareableMassaDBController) {
+    todo!();
+}
+
 fn main() {
-    // mirror massa ledger setup
     let args = Args::from_args();
-    let config = LedgerConfig {
-        thread_count: THREAD_COUNT,
-        disk_ledger_path: args.path,
-        initial_ledger_path: PathBuf::new(),
-        max_key_length: MAX_DATASTORE_KEY_LENGTH,
-        max_ledger_part_size: 0,
-        max_datastore_value_length: MAX_DATASTORE_VALUE_LENGTH,
-    };
-    let mut ledger = FinalLedger::new(config);
 
-    // edit section
-    let mut changes = LedgerChanges(PreHashMap::default());
-    changes.0.insert(
-        Address::from_str("AU12dhs6CsQk8AXFTYyUpc1P9e8GDf65ozU6RcigW68qfJV7vdbNf").unwrap(),
-        SetUpdateOrDelete::Set(LedgerEntry {
-            balance: Amount::from_mantissa_scale(100, 0),
-            bytecode: Bytecode(Vec::new()),
-            datastore: BTreeMap::default(),
-        }),
-    );
-    ledger.apply_changes(changes, Slot::min());
+    // Set up the following flags depending on what we want to do.
+    let convert_ledger = false;
+    let edit_ledger = false;
+    let scan_ledger = false;
 
-    // scan section
-    let balance = ledger.get_balance(
-        &Address::from_str("AU12AXf3ngq3e5zPWikXu3QGR18JtX3wbRuduA3126joowvPwVWdk").unwrap(),
-    );
-    println!("{:#?}", balance);
+    // Retrieve config structures
+    let db_config = get_db_config(args.path.clone());
+    let ledger_config = get_ledger_config(args.path.clone());
+    let final_state_config = get_final_state_config(args.path);
+    let mip_stats_config = get_mip_stats_config();
+
+    // Instantiate the main structs
+    let db = Arc::new(RwLock::new(
+        Box::new(MassaDB::new(db_config)) as Box<(dyn MassaDBController + 'static)>
+    ));
+    let ledger = FinalLedger::new(ledger_config, db.clone());
+    let mip_store =
+        MipStore::try_from((MIP_LIST, mip_stats_config)).expect("mip store creation failed");
+    let (selector_controller, _selector_receiver) = MockSelectorController::new_with_receiver();
+    let final_state = Arc::new(parking_lot::RwLock::new(
+        FinalState::new(
+            db.clone(),
+            final_state_config,
+            Box::new(ledger),
+            selector_controller.clone(),
+            mip_store,
+            true,
+        )
+        .expect("could not init final state"),
+    ));
+
+    // Edit section - Conversion from testnet22 to testnet23 ledger
+    if convert_ledger {
+        convert_from_testnet22_ledger_to_testnet23_ledger(db.clone());
+    }
+
+    // Edit section - Manual edits on the ledger or on the final_state
+    if edit_ledger {
+        let mut state_batch = DBBatch::new();
+        let versioning_batch = DBBatch::new();
+
+        // Here, we can create any state / versioning change we want
+        let mut changes = LedgerChanges(PreHashMap::default());
+        changes.0.insert(
+            Address::from_str("AU12dhs6CsQk8AXFTYyUpc1P9e8GDf65ozU6RcigW68qfJV7vdbNf").unwrap(),
+            SetUpdateOrDelete::Set(LedgerEntry {
+                balance: Amount::from_mantissa_scale(100, 0),
+                bytecode: Bytecode(Vec::new()),
+                datastore: BTreeMap::default(),
+            }),
+        );
+
+        // Apply the change to the batch
+        final_state
+            .write()
+            .ledger
+            .apply_changes_to_batch(changes, &mut state_batch);
+
+        // Write the batch to the DB
+        db.write().write_batch(state_batch, versioning_batch, None);
+    }
+
+    // Scan section
+    if scan_ledger {
+        // Here, we can query read functions from the state, but we could instead directly query the DB
+        let balance = final_state.read().ledger.get_balance(
+            &Address::from_str("AU12AXf3ngq3e5zPWikXu3QGR18JtX3wbRuduA3126joowvPwVWdk").unwrap(),
+        );
+        println!("{:#?}", balance);
+    }
 }
