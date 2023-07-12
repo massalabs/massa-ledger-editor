c30b582cb87bf93d7ec335d44dfdeac36e51b805
sydhds <sydhds@gmail.com>
2023-07-12 18:02:24 +0200

    Add unused import for mip list

--- src/versioning.rs
+++ src/versioning.rs
@@ -1,6 +1,16 @@
-use massa_versioning::versioning::{MipInfo, MipState};
+#[allow(unused_imports)]
+use std::collections::BTreeMap;
+
+#[allow(unused_imports)]
+use massa_time::MassaTime;
+
+#[allow(unused_imports)]
+use massa_versioning::versioning::{MipComponent, MipInfo, MipState};
+
 use massa_versioning::mips::get_mip_list as versioning_get_mip_list;
 
 pub fn get_mip_list() -> [(MipInfo, MipState); 0] {
     versioning_get_mip_list()
 }
+
+
